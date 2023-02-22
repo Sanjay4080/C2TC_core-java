0919d71ff3329bb65ae0c81fbfc188f09deb1383
Dell <Dell@SANJAY-2608>
2023-02-22 22:51:54 +0530

    first commit

---
+++ day1/src/datatypesdemo/Datatypedemo.java
@@ -0,0 +1,27 @@
+package datatypesdemo;
+
+public class Datatypedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int value1=9/2;
+System.out.println(value1);
+
+
+float value2=101/61;
+float a=4.5f;
+System.out.println(value2);
+System.out.println(a);
+
+
+double value3=10d/6d;
+double b=1.666666d;
+System.out.println(value3);
+System.out.println(b);
+
+int marker=512;
+float percentage = marker*0.4f;
+System.out.println(percentage);
+	}
+
+}
